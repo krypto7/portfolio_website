b7a3e20f4b3dcdffb71d87a87673368300f415b9
nishantkarekar <nishantkarekar98@gmail.com>
2025-03-14 19:02:35 +0530

    fix deprecated library issue

--- src/components/SmoothScroll/SmoothScroll.jsx
+++ src/components/SmoothScroll/SmoothScroll.jsx
@@ -1,5 +1,5 @@
+import ReactLenis from "lenis/react";
 import React from "react";
-import { ReactLenis } from "@studio-freight/react-lenis";
 
 function SmoothScroll({ children }) {
   const lenisOptions = {
