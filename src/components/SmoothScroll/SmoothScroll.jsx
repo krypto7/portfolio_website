82de0d15059077eb9028233dad824dadc54fafa9
nishantkarekar <nishantkarekar98@gmail.com>
2025-03-14 19:08:37 +0530

    fix bug

--- src/components/SmoothScroll/SmoothScroll.jsx
+++ src/components/SmoothScroll/SmoothScroll.jsx
@@ -1,18 +1,18 @@
-import ReactLenis from "lenis/react";
-import React from "react";
+// import ReactLenis from "lenis/react";
+// import React from "react";
 
-function SmoothScroll({ children }) {
-  const lenisOptions = {
-    lerp: 0.1, // control how smooth the scroll is
-    duration: 1.5, // slow down or speed up the scrolling
-    smoothTouch: false,
-    smooth: true,
-  };
-  return (
-    <ReactLenis root options={lenisOptions}>
-      {children}
-    </ReactLenis>
-  );
-}
+// function SmoothScroll({ children }) {
+//   const lenisOptions = {
+//     lerp: 0.1, // control how smooth the scroll is
+//     duration: 1.5, // slow down or speed up the scrolling
+//     smoothTouch: false,
+//     smooth: true,
+//   };
+//   return (
+//     <ReactLenis root options={lenisOptions}>
+//       {children}
+//     </ReactLenis>
+//   );
+// }
 
-export default SmoothScroll;
+// export default SmoothScroll;
--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import "../responsive.css";
-import SmoothScroll from "./components/smoothScroll/SmoothScroll.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <SmoothScroll>
-      <App />
-    </SmoothScroll>
+    {/* <SmoothScroll> */}
+    <App />
+    {/* </SmoothScroll> */}
   </React.StrictMode>
 );
