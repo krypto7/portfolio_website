8e0aab236439690ee77e5a1779605a2a8424e7da
nishantkarekar <nishantkarekar98@gmail.com>
2025-03-26 22:41:19 +0530

    add marquee for sticky footer

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import "../responsive.css";
-import SmoothScrolling from "./components/SmoothScroll/SmoothScroll.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <SmoothScrolling>
-      <App />
-    </SmoothScrolling>
+    <App />
   </React.StrictMode>
 );
